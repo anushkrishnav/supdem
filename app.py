4fc5d7daf6cfe2032b7887db88b8a0c6d278eb12
Vedant Khairnar <vedron007@gmail.com>
2021-01-12 15:30:17 +0530

    Basic DB Add

--- app.py
+++ app.py
@@ -12,12 +12,19 @@ migrate = Migrate(app, db)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
         return f"User('{self.password}')"
 
+class Supplier(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.password}')"
 
 @app.route("/")
 @app.route("/home")
@@ -32,3 +39,5 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
