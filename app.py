8f57ed38fab93f4f0eb21589bde9cf27ca14118e
Vedant Khairnar <vedron007@gmail.com>
2021-01-12 15:34:28 +0530

    fields made

--- app.py
+++ app.py
@@ -13,15 +13,17 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-
+    name = db.Column(db.String(60), nullable=False)
+    service = db.Column(db.String(60), nullable=False)
+    
     def __repr__(self):
         return f"User('{self.password}')"
 
 class Supplier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(60), nullable=False)
+    service  = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
         return f"User('{self.password}')"
