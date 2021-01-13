fa861826ee2f4dd6d48faa6882a0f6611ee47d99
anushkrishnav <anush.venkatakrishna@gmail.com>
2021-01-13 16:28:32 +0530

    added users and deleted db files

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo,  ValidationError
+from .app import User
 
 class UserForm(FlaskForm):
     id = StringField('id',validators=[DataRequired(), Length(min=2, max=20)])
