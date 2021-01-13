554e053d1bab555f37a2e0c56cc033ce3f0470c8
Vedant Khairnar <vedron007@gmail.com>
2021-01-13 15:50:25 +0530

    Front Updated

--- forms.py
+++ forms.py
@@ -10,6 +10,16 @@ class UserForm(FlaskForm):
     service = StringField('Service',validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
 
 class SupplierForm(FlaskForm):
     id = StringField('id',validators=[DataRequired()])
@@ -17,3 +27,13 @@ class SupplierForm(FlaskForm):
     name = StringField('Name',validators=[DataRequired()])
     service = StringField('Service',validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
