a022ab98fc21aa4c851695582e696857895af565
anushkrishnav <anush.venkatakrishna@gmail.com>
2021-02-24 02:59:48 +0530

    db upgrade

---
+++ migrations/versions/837eb2918b36_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 837eb2918b36
+Revises: 
+Create Date: 2021-02-24 02:50:15.749162
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '837eb2918b36'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
