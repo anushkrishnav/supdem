dc448b160c84f2cfd9840d79612a88fc2ef2c753
anushkrishnav <anush.venkatakrishna@gmail.com>
2021-02-24 04:02:32 +0530

    migration

---
+++ migrations/versions/d2580bd99191_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d2580bd99191
+Revises: 
+Create Date: 2021-02-24 03:57:25.686674
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd2580bd99191'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
